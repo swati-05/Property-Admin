46c6acc2f46c68f4a434204fab3b8ab4b51700ba
R-U-Bharti <kumarrubharti.rtcit@gmail.com>
2023-03-27 11:47:16 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from './App'
 import store from '@/Components/Redux/Store'
 import './index.css'
 import { Provider } from 'react-redux'
+import { useQuery, QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
