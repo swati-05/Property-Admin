56d9ffd335be5c72237273a2750b5aa8d61505bc
R-U-Bharti <kumarrubharti.rtcit@gmail.com>
2023-04-10 13:23:49 +0530

    added null to zero in cash verification

--- src/Pages/Property/CashVerification/CashVerification.jsx
+++ src/Pages/Property/CashVerification/CashVerification.jsx
@@ -8,7 +8,7 @@ import CashVerificationDetailedModal from './CashVerificationDetailedModal';
 import PropertyApiList from '@/Components/ApiList/PropertyApiList';
 import BarLoader from '@/Components/Common/BarLoader';
 import ApiHeader from '@/Components/ApiList/ApiHeader';
-import { nullToNA } from '@/Components/Common/PowerUps/PowerupFunctions';
+import { nullToNA, nullToZero } from '@/Components/Common/PowerUps/PowerupFunctions';
 import BottomErrorCard from '@/Components/Common/BottomErrorCard';
 
 const CashVerification = (props) => {
@@ -78,19 +78,19 @@ const CashVerification = (props) => {
         // },
         {
             Header: "Water",
-            Cell: ({ cell }) => (<span> ₹ {nullToNA(cell.row.original?.water)}</span>)
+            Cell: ({ cell }) => (<span> ₹ {nullToZero(cell.row.original?.water)}</span>)
         },
         {
             Header: "Trade",
-            Cell: ({ cell }) => (<span> ₹ {nullToNA(cell.row.original?.trade)}</span>)
+            Cell: ({ cell }) => (<span> ₹ {nullToZero(cell.row.original?.trade)}</span>)
         },
         {
             Header: "Total Amount",
-            Cell: ({ cell }) => (<span> ₹ {nullToNA(cell.row.original?.total)}</span>)
+            Cell: ({ cell }) => (<span> ₹ {nullToZero(cell.row.original?.total)}</span>)
         },
         {
             Header: "Paid Date",
-            Cell: ({ cell }) => (<span> ₹ {nullToNA(cell.row.original?.date)}</span>)
+            Cell: ({ cell }) => (<span> ₹ {nullToZero(cell.row.original?.date)}</span>)
 
         },
         // {
